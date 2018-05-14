893514be9cd81263ab3be4a3f4e08ebc3dd00042
tom-aglow <tom@ohhhh.me>
2018-05-14 21:42:52 +0300

    rename state property in Day component

--- src/components/PlanPage/Day/index.jsx
+++ src/components/PlanPage/Day/index.jsx
@@ -16,23 +16,23 @@ import './styles.css'
 
 class Day extends Component {
   state = {
-    tiles: {},
+    spots: {},
   }
 
   componentDidMount() {
     const { posts, slots } = this.props
     const slotsObj = slots.reduce(reduceToSlotsObject, {})
     const newPostObj = getNewPostObj()
-    this.setState({ tiles: { ...slotsObj, ...posts, ...newPostObj } })
+    this.setState({ spots: { ...slotsObj, ...posts, ...newPostObj } })
   }
 
   day = new Date(this.props.day)
 
   mapToSlotOrPost = key => {
-    const Element = this.state.tiles[key].type === 'slot' ? Slot : Post
+    const Element = this.state.spots[key].type === 'slot' ? Slot : Post
     return (
       <CSSTransition key={key} timeout={500} classNames="fade">
-        <Element {...this.state.tiles[key]} key={key} time={key} />
+        <Element {...this.state.spots[key]} key={key} time={key} />
       </CSSTransition>
     )
   }
@@ -45,23 +45,22 @@ class Day extends Component {
         text: 'Bazzz',
       },
     }
-    this.setState(prevState => ({ tiles: { ...prevState.tiles, ...foo } }))
+    this.setState(prevState => ({ spots: { ...prevState.spots, ...foo } }))
   }
 
   removePost = () => {
-    const [firstKey] = Object.keys(this.state.tiles).sort(sortStringsAsc)
-    const newTiles = Object.keys(this.state.tiles).reduce((result, key) => {
+    const [firstKey] = Object.keys(this.state.spots).sort(sortStringsAsc)
+    const newTiles = Object.keys(this.state.spots).reduce((result, key) => {
       if (key !== firstKey) {
-        result[key] = this.state.tiles[key]
+        result[key] = this.state.spots[key]
       }
       return result
     }, {})
-    console.log(newTiles)
-    this.setState({ tiles: newTiles })
+    this.setState({ spots: newTiles })
   }
 
   renderPosts() {
-    return Object.keys(this.state.tiles)
+    return Object.keys(this.state.spots)
       .sort(sortStringsAsc)
       .map(this.mapToSlotOrPost)
   }
