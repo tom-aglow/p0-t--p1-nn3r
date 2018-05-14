b7725dd6f37282d0efef327ab1ec0f32362b602d
tom-aglow <tom@ohhhh.me>
2018-05-14 18:55:12 +0300

    test list group transitions

--- src/components/PlanPage/Day/index.jsx
+++ src/components/PlanPage/Day/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import PropTypes from 'prop-types'
 
 import Stats from './Stats'
@@ -13,35 +14,77 @@ import {
 } from './utils'
 import './styles.css'
 
-const Day = ({ posts, stats, day: dayStr, slots }) => {
-  const day = new Date(dayStr)
-
-  const slotsObj = slots.reduce(reduceToSlotsObject, {})
-  const newPostObj = getNewPostObj()
-  const slotsAndPosts = { ...slotsObj, ...posts, ...newPostObj }
-
-  const mapToSlotOrPost = key => {
-    const Component = slotsAndPosts[key].type === 'slot' ? Slot : Post
-    return <Component {...slotsAndPosts[key]} key={key} time={key} />
+class Day extends Component {
+  state = {
+    tiles: {},
   }
 
-  const renderPosts = () =>
-    Object.keys(slotsAndPosts)
-      .sort(sortStringsAsc)
-      .map(mapToSlotOrPost)
+  componentDidMount() {
+    const { posts, slots } = this.props
+    const slotsObj = slots.reduce(reduceToSlotsObject, {})
+    const newPostObj = getNewPostObj()
+    this.setState({ tiles: { ...slotsObj, ...posts, ...newPostObj } })
+  }
 
-  return (
-    <section className="Day">
-      <header className="Day__header">
-        <h1 className="Day__title">
-          {formatDay(day)}{' '}
-          <span className="Day__dayname">{getWeekdayName(day)}</span>
-        </h1>
-        {stats && <Stats params={stats} />}
-      </header>
-      <div className="Day__content">{renderPosts()}</div>
-    </section>
-  )
+  day = new Date(this.props.day)
+
+  mapToSlotOrPost = key => {
+    const Element = this.state.tiles[key].type === 'slot' ? Slot : Post
+    return (
+      <CSSTransition key={key} timeout={500} classNames="fade">
+        <Element {...this.state.tiles[key]} key={key} time={key} />
+      </CSSTransition>
+    )
+  }
+
+  addPost = () => {
+    const d = new Date()
+    const foo = {
+      [`${d.getHours()}:${d.getSeconds()}`]: {
+        media: ['facebook', 'twitter'],
+        text: 'Bazzz',
+      },
+    }
+    this.setState(prevState => ({ tiles: { ...prevState.tiles, ...foo } }))
+  }
+
+  removePost = () => {
+    const [firstKey] = Object.keys(this.state.tiles).sort(sortStringsAsc)
+    const newTiles = Object.keys(this.state.tiles).reduce((result, key) => {
+      if (key !== firstKey) {
+        result[key] = this.state.tiles[key]
+      }
+      return result
+    }, {})
+    console.log(newTiles)
+    this.setState({ tiles: newTiles })
+  }
+
+  renderPosts() {
+    return Object.keys(this.state.tiles)
+      .sort(sortStringsAsc)
+      .map(this.mapToSlotOrPost)
+  }
+
+  render() {
+    const { stats } = this.props
+    return (
+      <section className="Day">
+        <header className="Day__header">
+          <h1 className="Day__title">
+            {formatDay(this.day)}{' '}
+            <span className="Day__dayname">{getWeekdayName(this.day)}</span>
+          </h1>
+          {stats && <Stats params={stats} />}
+        </header>
+        <TransitionGroup className="Day__content">
+          {this.renderPosts()}
+        </TransitionGroup>
+        <button onClick={this.addPost}>add</button>
+        <button onClick={this.removePost}>delete</button>
+      </section>
+    )
+  }
 }
 
 Day.defaultProps = {
