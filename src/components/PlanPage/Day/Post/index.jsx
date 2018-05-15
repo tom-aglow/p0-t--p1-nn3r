fc3beea9f6c7a52259b78e73f2bcd8002ef400fa
tom-aglow <tom@ohhhh.me>
2018-05-15 09:15:12 +0300

    make context for the page

--- src/components/PlanPage/Day/Post/index.jsx
+++ src/components/PlanPage/Day/Post/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Icon from 'components/PlanPage/shared/Icon'
+import Consumer from 'components/PlanPage/consumer'
 
 import { formatTime } from '../utils'
 import icons from './icons'
@@ -11,14 +12,27 @@ const Post = ({ media, text, time }) => {
 
   const renderMediaIcon = () => media.map(mapToIconElement)
 
+  const handleClick = cb => () => {
+    cb()
+    console.log('hey')
+  }
+
   return (
-    <article className="Post __tile__">
-      <header className="Post__header">
-        <span className="Post__time">{formatTime(time)}</span>
-        <div className="Post__socialmedia">{renderMediaIcon()}</div>
-      </header>
-      <p>{text}</p>
-    </article>
+    <Consumer>
+      {({ onTileClick }) => (
+        <article
+          className="Post __tile__"
+          onClick={handleClick(onTileClick)}
+          role="button"
+        >
+          <header className="Post__header">
+            <span className="Post__time">{formatTime(time)}</span>
+            <div className="Post__socialmedia">{renderMediaIcon()}</div>
+          </header>
+          <p>{text}</p>
+        </article>
+      )}
+    </Consumer>
   )
 }
 
--- src/components/PlanPage/index.jsx
+++ src/components/PlanPage/index.jsx
@@ -1,19 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Loader from './Loader'
 import Day from './Day'
 import './styles.css'
 
-const PlanPage = props => {
-  const { data } = props
-  const hasData = Object.keys(data).length > 0
+const { Provider, Consumer } = React.createContext()
 
-  const mapToDayComponent = day => (
-    <Day posts={data[day].posts} stats={data[day].stats} key={day} day={day} />
-  )
+class PlanPage extends Component {
+  onTileClick = () => {
+    console.log('doohh')
+  }
 
-  const renderDays = () => Object.keys(data).map(mapToDayComponent)
+  state = {
+    context: {
+      onTileClick: this.onTileClick,
+    },
+  }
 
-  return <div className="PlanPage">{hasData ? renderDays() : <Loader />}</div>
+  renderDays() {
+    const { data } = this.props
+    const mapToDayComponent = day => (
+      <Day
+        posts={data[day].posts}
+        stats={data[day].stats}
+        key={day}
+        day={day}
+      />
+    )
+
+    return Object.keys(data).map(mapToDayComponent)
+  }
+
+  render() {
+    const { data } = this.props
+    const { context } = this.state
+    const hasData = Object.keys(data).length > 0
+
+    return (
+      <Provider value={context}>
+        <div className="PlanPage">
+          {hasData ? this.renderDays() : <Loader />}
+        </div>
+      </Provider>
+    )
+  }
 }
 
 export default PlanPage
+export { Consumer }
+
+// const PlanPage = props => {
+//   const { data } = props
+//   const hasData = Object.keys(data).length > 0
+//
+//   const mapToDayComponent = day => (
+//     <Day posts={data[day].posts} stats={data[day].stats} key={day} day={day} />
+//   )
+//
+//   const renderDays = () => Object.keys(data).map(mapToDayComponent)
+//
+//   return <div className="PlanPage">{hasData ? renderDays() : <Loader />}</div>
+// }
